c80fa82b5820daab9e63e4c61e30094a3dd1a7da
bramlefevre@gmail.com <bramlefevre@gmail.com@a095b4de-43f9-48f7-2ed0-d256f33217d0>
2014-12-23 14:23:39 +0000

    Role start User management Init return url fix

---
+++ trunk/RiverCruise/RiverCruise/Models/UserManagement/UsersViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Security;
+using RiverCruise.Helpers;
+using WebMatrix.WebData;
+
+namespace RiverCruise.Models.UserManagement
+{
+    public class UsersViewModel
+    {
+        public UsersViewModel(IEnumerable<object> users, IEnumerable<string> editusers)
+        {
+            IList<dynamic> usersList = users as IList<dynamic> ?? users.ToList();
+
+            var editUsersList = editusers.ToList();
+
+            Users = new List<UserViewModel>();
+            foreach (dynamic o in usersList)
+            {
+                Users.Add(new UserViewModel()
+                {
+                    UserId = o["UserId"],
+                    UserName = o["UserName"],
+                    CanEdit = editUsersList.Contains(o["UserName"])
+                });
+            }
+        }
+
+        public List<UserViewModel> Users { get; set; }
+    }
+}
