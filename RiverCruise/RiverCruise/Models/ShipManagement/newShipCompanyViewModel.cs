7f9a7c0731fa2c8d1ad2b14bc596159960c96e0f
bramlefevre@gmail.com <bramlefevre@gmail.com@a095b4de-43f9-48f7-2ed0-d256f33217d0>
2014-12-24 15:01:42 +0000

    - Initial AddShip

--- RiverCruise/RiverCruise/Models/ShipManagement/newShipCompanyViewModel.cs
+++ RiverCruise/RiverCruise/Models/ShipManagement/newShipCompanyViewModel.cs
@@ -1,7 +1,10 @@
-﻿namespace RiverCruise.Models.ShipManagement
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RiverCruise.Models.ShipManagement
 {
     public class NewShipCompanyViewModel
     {
+        [Display(Name = "Company name")]
         public string Name { get; set; }
         public int Id { get; set; }
     }
