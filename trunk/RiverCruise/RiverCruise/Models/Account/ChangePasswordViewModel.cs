def78b8cf7ca8d1202276d06aafe3f9b5e272462
bramlefevre@gmail.com <bramlefevre@gmail.com@a095b4de-43f9-48f7-2ed0-d256f33217d0>
2014-12-24 13:00:21 +0000

    - Change password

---
+++ trunk/RiverCruise/RiverCruise/Models/Account/ChangePasswordViewModel.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+using System.Web.Security;
+using WebMatrix.WebData;
+
+namespace RiverCruise.Models.Account
+{
+    public class ChangePasswordViewModel
+    {
+        public ValidationResult Validate()
+        {
+            var result = new ValidationResult();
+            
+            if (NewPassword.Equals(NewPasswordConfirm))
+            {
+                result.Valid = true;
+            }
+            else
+            {
+                result.Valid = false;
+                result.ErrorMessages.Add("Passwords do not match");
+            }
+
+            return result;
+        }
+
+        [Display(Name = "Confirm new password")]
+        [Required]
+        public string NewPasswordConfirm { get; set; }
+
+        [Display(Name = "New Password")]
+        [Required]
+        public string NewPassword { get; set; }
+
+        [Display(Name = "Old password")]
+        [Required]
+        public string OldPassword { get; set; }
+
+        public bool PasswordChanged { get; set; }
+    }
+
+    public class ValidationResult
+    {
+        public bool Valid { get; set; }
+
+        public List<string> ErrorMessages { get; set; }
+
+        public ValidationResult()
+        {
+            ErrorMessages = new List<string>();
+        }
+    }
+}
