d4b560472bcb346e49ac2323ef2d446ef04d04b8
likkledwagon <bram.lefevre@gmail.com>
2015-03-14 17:08:04 +0100

    - Allow html in advalvas

--- RiverCruise/RiverCruise/Models/Advalvas/AddAdvalvasModel.cs
+++ RiverCruise/RiverCruise/Models/Advalvas/AddAdvalvasModel.cs
@@ -1,11 +1,12 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
 
 namespace RiverCruise.Models.Advalvas
 {
     public class AddAdvalvasModel
     {
         [Required]
-        [DataType(DataType.MultilineText)]
+        [AllowHtml]
         public string Title { get; set; }
 
         public Data.ProxyModel.Advalvas.AddAdvalvasModel ToDataModel()
